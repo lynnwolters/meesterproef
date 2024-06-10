d9f45cfe20f39edfe947b9025c8ae3d7e9d92ead
Stephan <stephan.s.p@outlook.com>
2024-06-10 11:17:33 +0200

    aparte css bestanden

--- page-de-man-met-de-hamer.php
+++ page-de-man-met-de-hamer.php
@@ -1,44 +1,43 @@
 <?php get_header(); ?>
 
-<section class="scene__room">
-                <div class="scene__room__plane__front"></div>
-                <div class="scene__room__plane__back"></div>
-                <div class="scene__room__plane__left"></div>
-                <div class="scene__room__plane__right">
+    <section class="scene__room">
+        <div class="scene__room__plane__front"></div>
+        <div class="scene__room__plane__back"></div>
+        <div class="scene__room__plane__left"></div>
+        <div class="scene__room__plane__right">
 
-                    <button class="scene__menu">
-                        <div class="scene__menu__plane__front">
-                            <p>Menu</p>
-                            <ul class="scene__menu__plane__front__submenu">
-                                <li>
-                                    <a class="active-link" href="#">Home</a>
-                                </li>
-                                <li>
-                                    <a href="#">Over Ons</a>
-                                </li>
-                                <li>
-                                    <a href="#">Shop</a>
-                                </li>
-                                <li>
-                                    <a href="#">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="scene__menu__plane__back"></div>
-                        <div class="scene__menu__plane__left">
-                            <img src="http://man-met-de-hamer.local/wp-content/uploads/2024/06/logo-mmdh.png" alt="Het logo van De Man Met De Hamer">
-                            <div class="scene__menu__plane__left__extension"></div>
-                        </div>
-                        <div class="scene__menu__plane__right"></div>
-                        <div class="scene__menu__plane__top"></div>
-                        <div class="scene__menu__plane__bottom"></div>
-                    </button>
-                    
+        <!-- menu button -->
+            <button class="scene__menu">
+                <div class="scene__menu__plane__front">
+                    <p>Menu</p>
+                    <ul class="scene__menu__plane__front__submenu">
+                        <li>
+                            <a class="active-link" href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Over Ons</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="scene__room__plane__top"></div>
-                <div class="scene__room__plane__bottom"></div>
-</section>
-
-
+                <div class="scene__menu__plane__back"></div>
+                <div class="scene__menu__plane__left">
+                    <img src="http://man-met-de-hamer.local/wp-content/uploads/2024/06/logo-mmdh.png" alt="Het logo van De Man Met De Hamer">
+                    <div class="scene__menu__plane__left__extension"></div>
+                </div>
+                <div class="scene__menu__plane__right"></div>
+                <div class="scene__menu__plane__top"></div>
+                <div class="scene__menu__plane__bottom"></div>
+            </button>
+            
+        </div>
+        <div class="scene__room__plane__top"></div>
+        <div class="scene__room__plane__bottom"></div>
+    </section>
 
 <?php get_footer(); ?>

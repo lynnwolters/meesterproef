3aaa01140484a396b1688f7bf0df0e0a3f1cdb29
Eva Boogaard <evaboogaard@hotmail.com>
2024-06-13 09:58:59 +0200

    view transitions

--- page-de-man-met-de-hamer.php
+++ page-de-man-met-de-hamer.php
@@ -98,7 +98,7 @@
 
                 ?>
                     <li class="item">
-                        <button href="#"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="750x500x1"></button>
+                        <button href="#"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="" loading="lazy"></button>
                         <div class="data_pop-up" data-title="<?php the_title(); ?>" data-url="<?php the_permalink(); ?>" data-year="<?php echo esc_html($year); ?>" data-photographer="<?php echo esc_html($photographer); ?>" data-ontwerper="<?php echo esc_html($ontwerper); ?>" data-images='<?php echo json_encode(array_column($images, 'url')); ?>'>
                             <!-- Store image URLs in a data attribute -->
                         </div>

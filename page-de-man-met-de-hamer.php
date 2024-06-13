457285ca712f6c860cbf783ce252e7ecffd6bca4
Stephan <stephan.s.p@outlook.com>
2024-06-13 14:09:27 +0200

    over ons pagina styling

--- page-de-man-met-de-hamer.php
+++ page-de-man-met-de-hamer.php
@@ -39,11 +39,19 @@
     </button>
 
     <section id="over-ons">
-        <div class="over-ons-text">
-            <h2>Ontstaan</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum.</p>
-            <h2>Visie</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut dolore quod! Cupiditate, consequatur tempora deleniti adipisci quae aspernatur sequi quod neque vero eos praesentium sit molestias voluptates similique laborum.</p>
+    <div class="over-ons-text">
+            <h2>Over ons</h2>
+            <p>Welkom bij De Man met de Hamer! Wij zijn een enthousiast team met een passie voor vakmanschap en oog voor detail. Of het nu gaat om indrukwekkende theaterdecors, unieke props of interieur, wij staan klaar om je ideeën werkelijkheid te maken.
+Bij De Man met de Hamer geloven we dat een goed ontwerp een verhaal vertelt en een blijvende indruk achterlaat. Met jarenlange ervaring en een breed scala aan vaardigheden, combineren we traditionele technieken met moderne innovaties om producten te maken die zowel functioneel als mooi zijn.
+We begeleiden je door het hele proces om ervoor te zorgen dat het eindresultaat precies is wat je voor ogen had.
+Geef ons een belletje en kom langs in onze werkplaats in Diemen om te ontdekken hoe De Man met de Hamer jouw volgende project tot een meesterwerk kan maken. We kunnen niet wachten om samen aan de slag te gaan!</p>
+        </div>
+        
+        <div class="vrienden-text">
+            <h2>Vrienden van De Man Met De Hamer</h2>
+            <p>In de vele jaren werkervaring heeft De Man met de Hamer veel waardevolle contacten gelegd. Deze vrienden van ons zijn stuk voor stuk experts in hun vakgebied, en we bevelen ze van harte aan voor jouw projecten</p>
+            <h2>Medewerkers</h2>
+            <p>Bij De Man met de Hamer zijn we trots op ons team van getalenteerde en toegewijde medewerkers. Elk lid van ons team brengt unieke vaardigheden met zich mee, waardoor we samen de hoogste kwaliteit kunnen leveren.</p>
         </div>
     </section>
 
